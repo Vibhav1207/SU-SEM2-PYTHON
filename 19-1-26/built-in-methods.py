d9bbba15aefcab30823ee79a905c93a32ec37248
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2026-01-23 19:08:23 +0530

    a

---
+++ 19-1-26/built-in-methods.py
@@ -0,0 +1,12 @@
+a = "sanjivani"
+print(len(a))
+
+print("----------------------")
+
+a = 12
+b = 12
+print(sum((a, b)))
+
+print("----------------------")
+a = "DBAEFC"
+print(sorted(a))
--- 19-1-26/str.py
+++ 19-1-26/str.py
@@ -1,2 +1,18 @@
 a = "sanjivani"
-sorted
+b = '''Vibhav'''
+c = """Hello World"""
+d = 'Python Programming'
+e = str(1234567)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print ("----------------------")
+a = "Sanjivani"
+print(a[5])
+
+print ("----------------------")
+a= "Hello World"
+for i in a:
+    print(i, end=" ")
